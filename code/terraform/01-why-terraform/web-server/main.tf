2667820874b719f2c91bcf5c2fd5fac60b62cad6
Cuong Truong Hoang Nam (VN) <Cuong.TruongHN@homecredit.vn>
2019-12-16 17:26:54 +0700

    cba

--- code/terraform/01-why-terraform/web-server/main.tf
+++ code/terraform/01-why-terraform/web-server/main.tf
@@ -14,7 +14,6 @@ resource "aws_instance" "example" {
   availability_zone = "ap-southeast-1a"
   ami               = "ami-07539a31f72d244e7"
   vpc_security_group_ids = ["${aws_security_group.instance.id}"]
-  subnet_id="subnet-09981c4e74eb3a574"
   user_data = <<-EOF
               #!/bin/bash
               sudo service apache2 start
@@ -26,6 +25,7 @@ resource "aws_instance" "example" {
 
 resource "aws_security_group" "instance" {
   name = "terraform-example-"
+  vpc_id="vpc-084640cab619447f7"  
   ingress{
     from_port = 8080
     to_port = 8080
